bca34a000893feb16f5052789af1224131b85a4c
SORO <kolosiaka@gmail.com>
2023-01-30 17:18:16 +0100

    'w6d1'

---
+++ ExerciceXP.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>ExerciceXP</title>
+</head>
+<body>
+    <?php
+        $hello = 'Hello PHP<br>';
+        $morning = 'Good Morning';
+        $nice = 'Have a nice day!'; 
+        echo "Hello World <br>";
+        echo $hello;
+        echo $morning . '.'.$nice.'<br>' ;
+        echo strrev('hello classe');
+
+
+
+     ?>
+</body>
+</html>
